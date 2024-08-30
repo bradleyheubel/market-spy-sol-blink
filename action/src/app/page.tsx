0532325e3c74da7ee9d1885dcf00e3cddb9d3a51
Bradley Heubel <bradley.heubel@gmail.com>
2024-08-30 17:33:39 +1000

    Update page.tsx

--- action/src/app/page.tsx
+++ action/src/app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>This is a Solana blink.</p>
-      <a href="https://donate.marketspy.au/" target="_blank" rel="noopener noreferrer"></a>
+      <p>Donations welcome to wallet address: 4ypD7kxRj9DLF3PMxsY3qvp8YdNhAHZRnN3fyVDh5CFX</p>
+      <p>This is also a Solana blink.</p>
     </main>
   );
 }
